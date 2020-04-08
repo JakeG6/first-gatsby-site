ca0504227c633f18dd61e826aa432c36e3d67a54
Jacob <jakeguss@gmail.com>
2020-04-07 22:16:10 -0600

    initialized footer

---
+++ src/components/footer.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <div>
+            <Link className="footerLink" to="/page-2/">Go to page 2</Link>
+            <Link className="footerLink" to="/blog/">Go to the blog</Link>
+            <Link className="footerLink" to="/contact/">Contact</Link>
+            <div>
+                © {new Date().getFullYear()}, Built with
+                {` `}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </div>
+            
+        </div>
+        
+    )
+}
+
+export default Footer;
